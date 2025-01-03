4d3d77e543a8631b99649323a7614829059d46dd
Jeremiah-com <sosimitosin@gmail.com>
2025-01-03 13:44:20 +0000

    Add files via upload

---
+++ classwork.js
@@ -0,0 +1,14 @@
+// creating an array to hold customer names
+
+let customers = ["Ama", "Venas", "Jerry", "Paolo", "Kweku"]
+
+// using for loop to get the list and printing names except kweku
+
+for (let j = 0; j < customers.length; j++)
+{
+    if (customers[j] !== "Kweku"){
+    console.log (customers[j]); }
+}
+
+
+
